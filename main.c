d84e84607d4400322691987cce12996ba8852584
Tomas Lajda <tomas.lajda0@gmail.com>
2023-09-20 10:48:24 +0200

    added a queue to linked list

--- main.c
+++ main.c
@@ -1,53 +1,69 @@
 #include <stdio.h>
 #include <stdlib.h>
 
- struct Node  {
+struct Node {
     int number;
-    struct Node* next;
-} ;
+    struct Node *next;
+};
 
-struct Node* head = NULL;
+struct Node *head = NULL;
 
 void createNode(int data) {
-    struct Node* newNode = malloc(sizeof(struct Node));
-    newNode->number = data;
-    if(head == NULL){
-        head = newNode;
-        newNode->next = newNode;
-    }
-    else {
-        newNode->next = head->next;
-        head->next = newNode;
-        head = newNode;
-    }
+  struct Node *newNode = malloc(sizeof(struct Node));
+  newNode->number = data;
+  if (head == NULL) {
+    head = newNode;
+    newNode->next = newNode;
+  } else {
+    newNode->next = head->next;
+    head->next = newNode;
+    head = newNode;
+  }
+}
+
+void dequeue() {
+  struct Node *lastNode = head;
+
+  if (lastNode == NULL) return;
+
+  struct Node *currentNode = head->next;
+
+  if (currentNode == NULL) free(lastNode);
+  else {
+    lastNode->next = currentNode->next;
+    free(currentNode);
+  }
 }
 
 void printNodeList() {
-    struct Node* currentNode = head;
-    struct Node* lastNode = currentNode;
-    do{
-        printf("%i\t", currentNode->number);
-        currentNode = currentNode-> next;
-    } while (currentNode != lastNode);
+  struct Node *currentNode = head;
+  struct Node *lastNode = currentNode;
+
+  if (currentNode == NULL) return;
+
+  do {
+    printf("%i\t", currentNode->number);
+    currentNode = currentNode->next;
+  } while (currentNode != lastNode);
 }
 
 
 int main() {
-    char continueAdding = 'y';
-    do {
-        int input;
-        printf("Please type a integer value: ");
-        scanf("%d", &input);
-        fflush(stdin);
+  char continueAdding = 'y';
+  do {
+    int input;
+    printf("Please type a integer value: ");
+    scanf("%d", &input);
+    fflush(stdin);
 
-        createNode(input);
+    createNode(input);
 
-        printf("Do you want to continue? y/n\n");
-        scanf("%c", &continueAdding);
-        fflush(stdin);
-    } while(continueAdding == 'y');
+    printf("Do you want to continue? y/n\n");
+    scanf("%c", &continueAdding);
+    fflush(stdin);
+  } while (continueAdding == 'y');
 
-    printNodeList();
+  printNodeList();
 
-    return 0;
+  return 0;
 }
